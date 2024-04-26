470fb4ba3a71997b42915f09e43c63c1c732e825
David Battefeld <davidbattefeld@gmail.com>
2024-04-26 23:20:28 +0200

    fix: use production URL for Skylarks website

--- app/src/main/java/de/davidbattefeld/berlinskylarks/classes/api/SkylarksAPIRequest.kt
+++ app/src/main/java/de/davidbattefeld/berlinskylarks/classes/api/SkylarksAPIRequest.kt
@@ -7,8 +7,7 @@ import io.ktor.http.URLBuilder
 import io.ktor.http.headers
 
 abstract class SkylarksAPIRequest: AbstractAPIRequest() {
-    //TODO: change to live once available
-    override val API_URL = "https://dev.tib-baseball.de"
+    override val API_URL = "https://tib-baseball.de"
     override val authKey = AUTH_HEADER
 
     override fun URLBuilder.addAuthorizationParameters() {}
