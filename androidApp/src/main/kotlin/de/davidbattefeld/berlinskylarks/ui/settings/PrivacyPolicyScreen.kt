bdc7b7ce9f6c058fd82e18fe06ddc3b572b02d17
David Battefeld <post@davidbattefeld.de>
2025-09-22 21:13:07 +0200

    polish(android): fix colors in legal routes

--- androidApp/src/main/kotlin/de/davidbattefeld/berlinskylarks/ui/settings/PrivacyPolicyScreen.kt
+++ androidApp/src/main/kotlin/de/davidbattefeld/berlinskylarks/ui/settings/PrivacyPolicyScreen.kt
@@ -3,8 +3,10 @@ package de.davidbattefeld.berlinskylarks.ui.settings
 import android.widget.TextView
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
@@ -15,15 +17,22 @@ import de.davidbattefeld.berlinskylarks.ui.viewmodels.SettingsViewModel
 fun PrivacyPolicyScreen(vm: SettingsViewModel = viewModel()) {
     val context = LocalContext.current
     val displayedText = vm.readStaticMarkdownFile("app_pp_en.md", context)
+    val textColor = MaterialTheme.colorScheme.onSurface.toArgb()
+    val backgroundColor = MaterialTheme.colorScheme.surface.toArgb()
 
     LazyColumn(
         modifier = Modifier
-            .padding(horizontal = 12.dp),
+            .padding(horizontal = 16.dp),
     ) {
         item {
             AndroidView(
-                factory = { TextView(it) },
-                update = { it.text = displayedText }
+                factory = {
+                    TextView(it).apply {
+                        text = displayedText
+                        setTextColor(textColor)
+                        setBackgroundColor(backgroundColor)
+                    }
+                },
             )
         }
     }
