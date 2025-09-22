9f8cc288bcb13ef883daefecb1cbb22812a76bed
David Battefeld <post@davidbattefeld.de>
2025-09-22 20:22:05 +0200

    chore(android): remove manual DI setup

--- androidApp/src/main/kotlin/de/davidbattefeld/berlinskylarks/data/BerlinSkylarksApplication.kt
+++ androidApp/src/main/kotlin/de/davidbattefeld/berlinskylarks/data/BerlinSkylarksApplication.kt
@@ -4,15 +4,4 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class BerlinSkylarksApplication : Application() {
-
-    /**
-     * AppContainer instance used by the rest of classes to obtain dependencies
-     */
-    lateinit var container: AppContainer
-
-    override fun onCreate() {
-        super.onCreate()
-        container = AppDataContainer(this)
-    }
-}
+class BerlinSkylarksApplication : Application()
