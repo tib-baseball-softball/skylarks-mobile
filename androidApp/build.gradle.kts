6f5881d59b6fc9bd6f850bab597b53aaddf1d904
David Battefeld <post@davidbattefeld.de>
2025-09-28 08:00:42 +0200

    build(android): install WorkManager

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -87,6 +87,7 @@ dependencies {
     implementation(libs.androidx.navigation3.runtime)
     implementation(libs.androidx.lifecycle.viewmodel.navigation3)
     implementation(libs.androidx.material3.adaptive.navigation3)
+    implementation(libs.androidx.work.runtime.ktx)
 
     testImplementation(libs.junit)
 
