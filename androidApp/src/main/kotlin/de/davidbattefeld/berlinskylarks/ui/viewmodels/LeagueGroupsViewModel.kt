094a714a14e5d9502ce8d05da9d3e4a5708110eb
David Battefeld <post@davidbattefeld.de>
2026-01-28 00:24:27 +0100

    polish: take selected season into account when displaying LeagueGroups

--- androidApp/src/main/kotlin/de/davidbattefeld/berlinskylarks/ui/viewmodels/LeagueGroupsViewModel.kt
+++ androidApp/src/main/kotlin/de/davidbattefeld/berlinskylarks/ui/viewmodels/LeagueGroupsViewModel.kt
@@ -4,28 +4,44 @@ import androidx.lifecycle.viewModelScope
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
 import dagger.hilt.android.lifecycle.HiltViewModel
+import de.berlinskylarks.shared.data.api.BSMAPIClient
 import de.berlinskylarks.shared.data.model.LeagueGroup
 import de.berlinskylarks.shared.database.repository.LeagueGroupRepository
 import de.davidbattefeld.berlinskylarks.data.repository.UserPreferencesRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 
+@OptIn(ExperimentalCoroutinesApi::class)
 @HiltViewModel(assistedFactory = LeagueGroupsViewModel.Factory::class)
 class LeagueGroupsViewModel @AssistedInject constructor(
     userPreferencesRepository: UserPreferencesRepository,
     leagueGroupRepository: LeagueGroupRepository,
 ) : GenericViewModel(userPreferencesRepository) {
-    var leagueGroups: StateFlow<List<LeagueGroup>> = leagueGroupRepository.getAllLeagueGroups()
-        .map { dbLeagueGroups ->
-            dbLeagueGroups.map { it.toLeagueGroup() }
+    val selectedSeason =
+        userPreferencesRepository.userPreferencesFlow.map { prefs ->
+            prefs.season
         }
-        .stateIn(
-            scope = viewModelScope,
-            started = SharingStarted.WhileSubscribed(stopTimeoutMillis = 5000),
-            initialValue = emptyList(),
-        )
+            .stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(stopTimeoutMillis = 5000),
+                initialValue = BSMAPIClient.DEFAULT_SEASON,
+            )
+
+    val leagueGroups: StateFlow<List<LeagueGroup>> =
+        selectedSeason.flatMapLatest { season ->
+            leagueGroupRepository.getLeagueGroupsBySeason(season).map { dbLeagueGroups ->
+                dbLeagueGroups.map { it.toLeagueGroup() }
+            }
+        }
+            .stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(stopTimeoutMillis = 5000),
+                initialValue = emptyList(),
+            )
 
     @AssistedFactory
     interface Factory {
