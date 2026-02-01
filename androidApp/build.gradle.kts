c2346e8a8c1dbb9275ee92df56ec2bc9631f65bd
David Battefeld <post@davidbattefeld.de>
2026-02-01 14:58:00 +0100

    build: bump version to 2.2.0

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -67,8 +67,8 @@ android {
         applicationId = "de.davidbattefeld.berlinskylarks"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 11
-        versionName = "2.1.0"
+        versionCode = 12
+        versionName = "2.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
